f1236b8a9a77eb8e3a3b3daaa96ebbfd2f552022
Gitdb <gitdb@localhost.com>
2021-09-23 12:29:57 -0500

    update

--- js/mondb.js
+++ js/mondb.js
@@ -47,3 +47,9 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+$("#cform").validate({
+    submitHandler: function(form) {
+      form.submit();
+    }
+   });
